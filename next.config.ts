8d771cc6fa60f4b14e1244bb740ba6536189fb6d
Ranjitpatra26 <ranjitspatra9077@gmail.com>
2026-09-02 15:36:17 +0530

    fix: add ui-avatars.com to next.config.ts image domains

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   allowedDevOrigins: ['192.168.56.1', 'localhost', '127.0.0.1'],
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'ui-avatars.com',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
