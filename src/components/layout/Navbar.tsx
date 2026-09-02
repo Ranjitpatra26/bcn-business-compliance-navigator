ab48be9b00f0cfb9a966fe44642143e6abf27702
Ranjitpatra26 <ranjitspatra9077@gmail.com>
2026-09-02 16:55:53 +0530

    fix: add missing null in ternary operator for navbar

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -182,7 +182,7 @@ export function Navbar() {
                   Get Started
                 </ParticleLink>
               </>
-            )}
+            ) : null}
           </div>
         </div>
       </div>
